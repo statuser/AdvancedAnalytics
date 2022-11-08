2c704a31cc63dadb4609fcef23ce05d4cfba4c76
John Howell <john.r.howell@gmail.com>
2022-11-08 15:06:01 -0700

    Basic Example Code

---
+++ ClassExamples/dataStructures.py
@@ -0,0 +1,19 @@
+class Product:
+    id = 0
+    name = "Item Name Here"
+    description = "A generic description"
+    quantity_on_hand = 0
+    price = 0.0
+    
+Product.id
+Product.name
+
+Product.id = 9991
+
+Product.id
+
+specific_product = Product()
+specific_product.quantity_on_hand
+specific_product.id = 2213
+Product.id
+specific_product.id
